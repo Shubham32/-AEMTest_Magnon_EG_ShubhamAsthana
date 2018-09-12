a112da896c5c592a03c9cba2875760abc81a983b
Shubham Asthana <sasthana2@sapient.com>
2018-09-13 01:06:04 +0530

    Changes in API Client

--- egplus/core/src/main/java/pom/core/api/APIClient.java
+++ egplus/core/src/main/java/pom/core/api/APIClient.java
@@ -110,6 +110,7 @@ public class APIClient {
 		outputString = StringUtils.replace(outputString, ")", "}");
 		LOGGER.debug("Websevice response string :: {}", outputString);
 		final ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
 		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		LOGGER.debug("Exit deserialize method");
 		return mapper.readValue(outputString, responseClassType);
